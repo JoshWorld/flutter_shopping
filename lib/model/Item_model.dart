9620109d1b730debffd074dd41c60479191e3e86
ukchanJung <ukchan@kakao.com>
2020-11-25 15:45:58 +0900

    Appbar Icon 구현

--- lib/model/Item_model.dart
+++ lib/model/Item_model.dart
@@ -4,10 +4,10 @@ class Item{
   int count = 0;
   bool check = false;
 
-  Item(this.name, this.count);
+  Item(this.name, this.price);
 
   @override
   String toString() {
-    return '$name 상품이 $num 원 이고 지금까지 $count팔렸습니다.';
+    return '$name 상품이 $price 원 이고 지금까지 $count개 팔렸습니다.';
   }
 }
--- lib/ui/itemList_page.dart
+++ lib/ui/itemList_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping/model/Item_model.dart';
 
 class ItemList extends StatefulWidget {
   @override
@@ -6,14 +7,52 @@ class ItemList extends StatefulWidget {
 }
 
 class _ItemListState extends State<ItemList> {
+  List<Item> items;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    items = [
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+      Item('서비스_1', 10000),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('아이템 리스트'),),
-      body: ListView(children: [
-
-      ],),
+      appBar: AppBar(
+        title: Text('아이템 리스트'),
+        actions: [
+          Row(
+            children: [
+              IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){}),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(items.where((item) => item.check==true).length.toString()),
+          )
+            ],
+          )   ,
+        ],
+      ),
+      body: ListView(
+          children: items
+              .map((e) => ListTile(
+                    title: Text(e.name),
+                    subtitle: Text(e.toString()),
+                  ))
+              .toList()),
     );
   }
 }
