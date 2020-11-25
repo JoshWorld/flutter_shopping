aeb0dc9d418625ee60bbda1d974d4539aaaea0d1
algorithm0x02 <algorithm0x02@gmail.com>
2020-11-25 19:02:02 +0900

    ui 작업 outline

---
+++ lib/item.dart
@@ -0,0 +1,14 @@
+class Item {
+  final String title;
+  final int price;
+
+  Item(this.title, this.price);
+}
+
+final itemList = [
+  Item("아이패드", 10000000),
+  Item("생존코딩 플래터", 32000),
+  Item("맥북 프로", 20000000),
+  Item("똘똘이(푸들)", 500000),
+  Item("강아지밥", 2000),
+];
